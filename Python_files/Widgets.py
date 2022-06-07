8b81425b17b2c81c962a0912b0fa828a51399c5b
ArS3Ny <93928890+ArS3Ny@users.noreply.github.com>
2022-06-07 12:22:28 +0300

    корректировка положения кнопок(чтобы они были по центру))

--- Python_files/Widgets.py
+++ Python_files/Widgets.py
@@ -75,7 +75,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MenuWithMenu()
+    ex = Spravka()
     ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec_())
