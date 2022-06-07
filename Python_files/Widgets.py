f2fef503d92b5dbccdfe45cd84f5aa7bc599fc11
ArS3Ny <93928890+ArS3Ny@users.noreply.github.com>
2022-06-07 16:41:19 +0300

    сделал(почти) профиль

--- Python_files/Widgets.py
+++ Python_files/Widgets.py
@@ -1,7 +1,9 @@
 import sys
-from PyQt5.QtGui import QPixmap, QPainter, QColor, QIcon
+from PyQt5.QtGui import QPixmap, QPainter, QColor, QIcon, QFont
 from PyQt5 import uic, QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QButtonGroup
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QButtonGroup,\
+    QVBoxLayout, QScrollArea
 
 
 class AnyWidget(QWidget):
@@ -18,8 +20,6 @@ class AnyWidget(QWidget):
 
         self.setWindowTitle(name)
         self.setFixedSize(1400, 800)
-
-
 
 
 class MenuWithMenu(AnyWidget):
@@ -74,6 +74,27 @@ class Menu(AnyWidget):
 class Profile(AnyWidget):
     def __init__(self):
         super().__init__('UI_files/Profile.ui', 'Профиль')
+        self.widget = QWidget()
+        self.vbox = QVBoxLayout()
+
+        for i in range(1, 50):
+            font = QFont('MS Shell Dlg 2', 30)
+            btn = QPushButton(f'Вариант {i}')
+            btn.setFont(font)
+            if i % 2 == 1:
+                btn.setStyleSheet('background-color: rgba(223, 116, 153, 150); color: '
+                                  'rgba(255, 255, 255, 150); text-align: left;')
+            else:
+                btn.setStyleSheet('background-color: rgba(158, 241, 162, 200); color: '
+                                  'rgba(255, 255, 255, 150); text-align: left;')
+            self.vbox.addWidget(btn)
+
+        self.vbox.setSpacing(20)
+        self.widget.setLayout(self.vbox)
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setWidget(self.widget)
 
 
 def except_hook(cls, exception, traceback):
@@ -82,7 +103,7 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Spravka()
+    ex = Profile()
     ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec_())
