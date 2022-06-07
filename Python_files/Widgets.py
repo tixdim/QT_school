bd25851649fb577bd7dc8ec60b371ba3b85b5592
Admin <tixdim.on1@gmail.com>
2022-06-07 13:32:21 +0300

    Commit

--- Python_files/Widgets.py
+++ Python_files/Widgets.py
@@ -1,25 +1,30 @@
 import sys
 from PyQt5.QtGui import QPixmap, QPainter, QColor, QIcon
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QLabel, QButtonGroup
 
 
 class AnyWidget(QWidget):
     def __init__(self, ui_file, name):
         super().__init__()
+
         self.img = QPixmap('1.jpg')
         self.imageLabel = QLabel(self)
         self.imageLabel.move(0, 0)
         self.imageLabel.resize(1400, 800)
         self.imageLabel.setPixmap(self.img)
+
         uic.loadUi(ui_file, self)
+
         self.setWindowTitle(name)
         self.setFixedSize(1400, 800)
 
 
+
+
 class MenuWithMenu(AnyWidget):
     def __init__(self):
-        super().__init__('menu_with_menu.ui', 'Меню (название переделать)')
+        super().__init__('UI_files/menu_with_menu.ui', 'Меню (название переделать)')
         self.Spravka_btn.clicked.connect(self.go_to_spravka)
         self.Menu_btn.clicked.connect(self.go_to_menu)
         self.Profile_btn.clicked.connect(self.go_to_profile)
@@ -49,24 +54,26 @@ class MenuWithMenu(AnyWidget):
 class Login(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('LogIn.ui', self)
-        img = QPixmap('1.jpg')
-        self.imageLabel.setPixmap(img)
+        uic.loadUi('UI_files/LogIn.ui', self)
+
+        self.back.setStyleSheet("background-color: #ffc0cb; border-radius: 25px;")
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
 
 
 class Spravka(AnyWidget):
     def __init__(self):
-        super().__init__('Spravka.ui', 'Справка')
+        super().__init__('UI_files/Spravka.ui', 'Справка')
 
 
 class Menu(AnyWidget):
     def __init__(self):
-        super().__init__('Menu.ui', 'Меню')
+        super().__init__('UI_files/Menu.ui', 'Меню')
 
 
 class Profile(AnyWidget):
     def __init__(self):
-        super().__init__('Profile.ui', 'Профиль')
+        super().__init__('UI_files/Profile.ui', 'Профиль')
 
 
 def except_hook(cls, exception, traceback):
