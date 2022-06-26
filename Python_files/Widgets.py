0fdfcf0ea8d2b75bb0e1d52f9aa82c10a40126af
ArS3Ny <93928890+ArS3Ny@users.noreply.github.com>
2022-06-26 23:49:29 +0300

    .

--- Python_files/Widgets.py
+++ Python_files/Widgets.py
@@ -51,7 +51,9 @@ class TheoryWidget(AnyWidget):
 
         self.nickname = nickname
 
-        for i in range(1, 59):
+        for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+                  27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+                  50, 51, 52, 53, 54, 55, 56, 57, 58]:
             pixmap = QPixmap(f'ege_po_borbe_theory/theory_{i}.png')
             with PIL.Image.open(f'ege_po_borbe_theory/theory_{i}.png') as img:
                 eval(f'self.label_{i}.setFixedSize({img.size[0]}, {img.size[1]})')
